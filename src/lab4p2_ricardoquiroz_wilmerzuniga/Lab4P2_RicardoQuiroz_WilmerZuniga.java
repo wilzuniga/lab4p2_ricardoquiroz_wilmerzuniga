06fc647d6a9bd253e005eb12f818f5757bdcec07
WilZuniga <wilmerzuniga.ant@gmail.com>
2022-08-12 17:07:39 -0600

    cambios en main

--- src/lab4p2_ricardoquiroz_wilmerzuniga/Lab4P2_RicardoQuiroz_WilmerZuniga.java
+++ src/lab4p2_ricardoquiroz_wilmerzuniga/Lab4P2_RicardoQuiroz_WilmerZuniga.java
@@ -11,19 +11,25 @@ public class Lab4P2_RicardoQuiroz_WilmerZuniga {
         Object Tablero[][] = new Object[8][8];
 
         Iniciar(Tablero);
-        impremarG(Tablero);
+        
+        System.out.println("La palabra jaque mate proviene de las palabras persas sha mat, que significan -El rey ha fallecido- ");
 
-        while (ciclo == false) {
-            System.out.println("-----------------------------------------------");
-            System.out.println("Ingrese el nombre de el jugador uno: ");
-            lea = new Scanner(System.in);
-            String P1 = lea.nextLine();// Piezas blancas
-            System.out.println("");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Ingrese el nombre de el jugador uno: ");
+        lea = new Scanner(System.in);
+        String P1 = lea.nextLine();// Piezas blancas
+        System.out.println("");
 
-            System.out.println("Ingrese el nombre de el jugador uno: ");
-            lea = new Scanner(System.in);
-            String P2 = lea.nextLine();// Piezas negras
-            System.out.println("");
+        System.out.println("Ingrese el nombre de el jugador uno: ");
+        lea = new Scanner(System.in);
+        String P2 = lea.nextLine();// Piezas negras
+        System.out.println("");
+        
+        while (ciclo == true) {
+            
+            
+            impremarG(Tablero);
+            
 
         }
 
@@ -42,8 +48,8 @@ public class Lab4P2_RicardoQuiroz_WilmerZuniga {
             System.out.println("");
         }
         
-        Tablero[0][0]= new Torre(0, 0, 'p');
-        Tablero[0][7] = new Torre(0, 7, 'p');
+        Tablero[0][0]= new Torre(0, 0, 'r');
+        Tablero[0][7] = new Torre(0, 7, 'r');
 
         Tablero[0][1] = new Caballo( 0, 1, 'n');
         Tablero[0][6] = new Caballo(0, 6, 'n');
@@ -51,9 +57,32 @@ public class Lab4P2_RicardoQuiroz_WilmerZuniga {
         Tablero[0][2] = new Alfil(0, 2, 'b');
         Tablero[0][5] = new Alfil(0, 5, 'b');
         
-        Tablero[0][3] = new Dama(0, 3, 'q');
+        Tablero[0][4] = new Dama(0, 3, 'q');
 
-        Tablero[0][4] = new Rey(0, 4, 'k');
+        Tablero[0][3] = new Rey(0, 4, 'k');
+        
+        for (int i = 0; i < 8; i++) {
+            Tablero[1][i] = new Peon(1, i, 'p');
+
+        }
+        
+        Tablero[7][0] = new Torre(7, 0, 'R');
+        Tablero[7][7] = new Torre(7, 7, 'R');
+
+        Tablero[7][1] = new Caballo(7, 1, 'N');
+        Tablero[7][6] = new Caballo(7, 6, 'N');
+
+        Tablero[7][2] = new Alfil(7, 2, 'B');
+        Tablero[7][5] = new Alfil(7, 5, 'B');
+
+        Tablero[7][4] = new Dama(7, 4, 'Q');
+
+        Tablero[7][3] = new Rey(7, 3, 'K');
+
+        for (int i = 0; i < 8; i++) {
+            Tablero[6][i] = new Peon(6, i, 'P');
+
+        }
 
 
 
@@ -81,6 +110,9 @@ public class Lab4P2_RicardoQuiroz_WilmerZuniga {
 
                 }else if(Tablero[i][j] instanceof Rey){
                     System.out.print("[" + ((Rey) Tablero[i][j]).getRep() + "]" + "    ");
+
+                }else if(Tablero[i][j] instanceof Peon){
+                    System.out.print("[" + ((Peon) Tablero[i][j]).getRep() + "]" + "    ");
 
                 }else{
                     System.out.print("[" + Tablero[i][j] + "]" + "    ");
