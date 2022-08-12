6e76560db7d23621b38b375267062176e9d0a5d0
rjquiroz02 <rjquiroz02@unitec.edu>
2022-08-12 13:41:52 -0600

    Clases

---
+++ src/lab4p2_ricardoquiroz_wilmerzuniga/Caballo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_ricardoquiroz_wilmerzuniga;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Caballo {
+    
+}
